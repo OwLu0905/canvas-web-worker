82b1df2601e64c5c35ab89c2d94f4d8addb2cc7e
OwLu0905 <owlu0905@gmail.com>
2023-10-09 22:37:22 +0800

    add dynamic color & hue when moving

--- src/lib/learn/Demo.svelte
+++ src/lib/learn/Demo.svelte
@@ -4,6 +4,7 @@
 
   const AMOUNT = 100;
   const particlesArray: Particle[] = [];
+  let hue = 0;
   onMount(() => {
     const canvas = document.getElementById("demo") as HTMLCanvasElement;
     const ctx = canvas.getContext("2d");
@@ -28,18 +29,52 @@
     canvas.addEventListener("click", function (event) {
       mouse.x = event.x;
       mouse.y = event.y;
-      // drawCircle(ctx, mouse.x, mouse.y);
+
+      for (let j = 0; j < 10; j++) {
+        particlesArray.push(
+          new Particle(
+            mouse,
+            {
+              width: canvas.width,
+              height: canvas.height,
+            },
+            hue
+          )
+        );
+      }
     });
 
     canvas.addEventListener("mousemove", function (event) {
       mouse.x = event.x;
       mouse.y = event.y;
       // drawCircle(ctx, mouse.x, mouse.y);
+
+      for (let j = 0; j < 5; j++) {
+        particlesArray.push(
+          new Particle(
+            mouse,
+            {
+              width: canvas.width,
+              height: canvas.height,
+            },
+            hue
+          )
+        );
+      }
     });
 
     function init() {
       for (let i = 0; i < AMOUNT; i++) {
-        particlesArray.push(new Particle(mouse));
+        particlesArray.push(
+          new Particle(
+            mouse,
+            {
+              width: canvas.width,
+              height: canvas.height,
+            },
+            hue
+          )
+        );
       }
     }
     init();
@@ -53,8 +88,14 @@
     mouse: Record<"x" | "y", number | undefined>
   ) {
     if (ctx) {
-      ctx.clearRect(0, 0, width, height);
+      // ctx.clearRect(0, 0, width, height);
+      ctx.fillStyle = "rgba(0,0,0,0.02)";
+      ctx.fillRect(0, 0, width, height);
       handleParticles(ctx);
+      hue += 0.5;
+      if (hue > 360) {
+        hue = 0;
+      }
       requestAnimationFrame(function () {
         animate(ctx, width, height, mouse);
       });
@@ -64,7 +105,12 @@
   function handleParticles(ctx: CanvasRenderingContext2D | null) {
     for (let i = 0; i < particlesArray.length; i++) {
       particlesArray[i].update();
-      particlesArray[i].draw(ctx);
+      particlesArray[i].draw(ctx, hue);
+      if (particlesArray[i].size <= 0.3) {
+        particlesArray.splice(i, 1);
+        // NOTE: due to mutate array length -> we need to adjust the index
+        i--;
+      }
     }
   }
 </script>
--- src/model/Particle.ts
+++ src/model/Particle.ts
@@ -1,27 +1,50 @@
 "use strict";
 class Particle {
-  x: number;
-  y: number;
+  x?: number;
+  y?: number;
   size: number;
   speedX: number;
   speedY: number;
-  constructor(mouse: Record<"x" | "y", number | undefined>) {
-    this.x = mouse.x ?? 0;
-    this.y = mouse.y ?? 0;
-    this.size = Math.random() * 5 + 1;
+  color: string;
+  constructor(
+    mouse: Record<"x" | "y", number | undefined>,
+    position: Record<"width" | "height", number>,
+    hue: number
+  ) {
+    this.x = mouse.x;
+    this.y = mouse.y;
+    // this.x = Math.random() * position.width;
+    // this.y = Math.random() * position.height;
+    this.size = Math.random() * 15 + 1;
     this.speedX = Math.random() * 3 - 1.5;
     this.speedY = Math.random() * 3 - 1.5;
+    this.color = "hsl(" + hue + ", 100%, 50%)";
   }
   update() {
+    if (!this.x || !this.y) return;
+    // if (this.x <= 0 || this.x >= window.innerWidth) {
+    //   this.speedX = -this.speedX;
+    // }
+    // if (this.y <= 0 || this.y >= window.innerHeight) {
+    //   this.speedY = -this.speedY;
+    // }
     this.x += this.speedX;
     this.y += this.speedY;
+
+    if (this.size > 0.2) {
+      this.size -= 0.1;
+    }
   }
-  draw(ctx: CanvasRenderingContext2D | null) {
+  draw(ctx: CanvasRenderingContext2D | null, hue: number = 0) {
     if (!ctx) return;
-    ctx.fillStyle = "gray";
+    if (!this.x || !this.y) return;
+    ctx.fillStyle = this.color;
+    // ctx.strokeStyle = "black";
+    // ctx.lineWidth = 1;
     ctx.beginPath();
-    ctx.arc(this.x, this.y, 5, 0, Math.PI * 2);
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
     ctx.fill();
+    // ctx.stroke();
   }
 }
 
